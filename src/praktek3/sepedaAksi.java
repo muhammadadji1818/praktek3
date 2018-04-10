4a18f018f44ecbba64cd109adf42cf0845542a17
muhammadadji1818 <35833891+muhammadadji1818@users.noreply.github.com>
2018-04-10 21:10:08 +0800

    instance,buat atrribut dan panggil method

--- src/praktek3/sepedaAksi.java
+++ src/praktek3/sepedaAksi.java
@@ -1,7 +1,12 @@
 package praktek3;
 public class sepedaAksi {
     public static void main(String[] args) {
+        sepeda s = new sepeda();
+        s.merk="kupu-kupu";
+        s.warna="merah";
+        s.harga=1000;
         
+        s.cetakinfo();
     }
     
 }
