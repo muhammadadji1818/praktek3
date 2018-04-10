21aa8868ee78d8c7d17d20521341a4ce609b9503
muhammadadji1818 <35833891+muhammadadji1818@users.noreply.github.com>
2018-04-10 21:23:50 +0800

    menggunakan method set

--- src/praktek3/sepedaAksi.java
+++ src/praktek3/sepedaAksi.java
@@ -2,9 +2,10 @@ package praktek3;
 public class sepedaAksi {
     public static void main(String[] args) {
         sepeda s = new sepeda();
-        s.merk="kupu-kupu";
-        s.warna="merah";
-        s.harga=1000;
+       
+        s.setMerk("butterfly");
+        s.setWarna("merah");
+        s.setHarga(1000);
         
         s.cetakinfo();
     }
