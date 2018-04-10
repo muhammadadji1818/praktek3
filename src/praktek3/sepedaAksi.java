343b960335f83074e0732ec942c82f32856a01a6
muhammadadji1818 <35833891+muhammadadji1818@users.noreply.github.com>
2018-04-10 21:32:55 +0800

    menggunakan method get

--- src/praktek3/sepedaAksi.java
+++ src/praktek3/sepedaAksi.java
@@ -8,6 +8,14 @@ public class sepedaAksi {
         s.setHarga(1000);
         
         s.cetakinfo();
+        
+        System.out.print("Merknya \t: ");
+        System.out.println(s.getMerk());
+        System.out.print("warnanya \t: ");
+        System.out.println(s.getWarna());
+        System.out.print("Harganya \t: ");
+        System.out.println(s.getHarga());
+        
     }
     
 }
