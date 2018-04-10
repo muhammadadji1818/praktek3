451ab0b7558b380b1d73bfb913d1cfde79e59892
muhammadadji1818 <35833891+muhammadadji1818@users.noreply.github.com>
2018-04-10 21:41:08 +0800

    getter and setter

--- src/praktek3/SepedaMotor.java
+++ src/praktek3/SepedaMotor.java
@@ -6,4 +6,14 @@ public class SepedaMotor extends sepeda{
     void throttle(){
         System.out.println("bremmmmm.......");
     }
+
+    public string getJenisMotor() {
+        return jenisMotor;
+    }
+
+    public void setJenisMotor(string jenisMotor) {
+        this.jenisMotor = jenisMotor;
+    }
+    
+    
 }
