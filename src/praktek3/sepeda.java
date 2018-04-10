788fc1225fe6b3e9c4d40412cadcb365443ff113
muhammadadji1818 <35833891+muhammadadji1818@users.noreply.github.com>
2018-04-10 21:16:33 +0800

    buat getter and setter

--- src/praktek3/sepeda.java
+++ src/praktek3/sepeda.java
@@ -11,4 +11,30 @@ public class sepeda {
                 "warna \t: "+warna+"\n"+
                 "warna \t: "+harga);
     }
+
+    public String getMerk() {
+        return merk;
+    }
+
+    public void setMerk(String merk) {
+        this.merk = merk;
+    }
+
+    public String getWarna() {
+        return warna;
+    }
+
+    public void setWarna(String warna) {
+        this.warna = warna;
+    }
+
+    public double getHarga() {
+        return harga;
+    }
+
+    public void setHarga(double harga) {
+        this.harga = harga;
+    }
+    
+    
 }
